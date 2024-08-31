5ac0d66842ad0903f1025621a310c7f65dd7b0ef
Ubuntu <ubuntu@ip-172-31-27-217.eu-north-1.compute.internal>
2024-08-31 17:50:53 +0000

    fixing tests

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,29 +14,7 @@ func TestPhotosEndpoint(t *testing.T) {
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		resp, err := http.Get("https://jsonplaceholder.typicode.com/photos")
-		if err != nil {
-			http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			http.Error(w, "Failed to read response", http.StatusInternalServerError)
-			return
-		}
-
-		var photos []Photo
-		if err := json.Unmarshal(body, &photos); err != nil {
-			http.Error(w, "Failed to parse JSON", http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(photos)
-	})
+	handler := http.HandlerFunc(getPhotosHandler)
 
 	handler.ServeHTTP(rr, req)
 
@@ -47,5 +24,9 @@ func TestPhotosEndpoint(t *testing.T) {
 
 	if rr.Header().Get("Content-Type") != "application/json" {
 		t.Errorf("handler returned wrong content type: got %v want %v", rr.Header().Get("Content-Type"), "application/json")
+	}
+
+	if !json.Valid(rr.Body.Bytes()) {
+		t.Errorf("handler returned invalid JSON: %s", rr.Body.String())
 	}
 }
